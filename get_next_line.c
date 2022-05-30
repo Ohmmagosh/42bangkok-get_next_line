74b18927a3c5bcf278858f50c3cab395166d71ed
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2022-05-30 23:34:40 +0700

    first-commit

---
+++ get_next_line.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 14:39:10 by psuanpro          #+#    #+#             */
+/*   Updated: 2022/05/30 21:47:14 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	 *get_next_line(int fd)
+{
+	char	*buff;
+	int	readbuffer;
+	int	i;
+
+	i = 0;
+	buff = malloc(sizeof(char) * 6);
+	if (fd < 0)
+		return (NULL);
+	while (i < 10)
+	{
+		readbuffer = read(fd, buff, 5);
+		printf("%d\n", readbuffer);
+		printf("%s\n", buff);
+		i++;
+	}
+	return ("hello welcome");
+}	
---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psuanpro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 14:39:39 by psuanpro          #+#    #+#             */
+/*   Updated: 2022/05/30 23:05:55 by psuanpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct get_next_line
+{
+	char	buffer;
+}	g_line;
+
+char	*get_next_line(int fd);
+
+
+
+#endif
