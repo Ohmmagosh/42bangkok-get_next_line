3f11dc62ed0780518c49b04810adc5cb638a235c
Phatcharaphol Suanprom <psuanpro@z3t4c1.42bangkok.com>
2022-06-19 04:45:16 +0700

    test

--- get_next_line.c
+++ get_next_line.c
@@ -6,128 +6,177 @@
 /*   By: psuanpro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 14:39:10 by psuanpro          #+#    #+#             */
-/*   Updated: 2022/06/09 03:00:32 by psuanpro         ###   ########.fr       */
+/*   Updated: 2022/06/19 04:45:00 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_strlen(char *s)
 {
-	size_t	i;
-
+	int	i;
+	
 	i = 0;
 	if (!s)
 		return (0);
-	while (s[i])
+	while (*(s + i))
 		i++;
 	return (i);
 }
 
-size_t	ft_len_nl(char *s)
+int	ft_len_nl(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (*s != '\n' && *s != '\0')
-	{
-		s++;
+	while (s != NULL && *(s + i) != '\0')
+	{	
+		if (*(s + i) == '\n')
+		{	
+			i++;
+			break ;
+		}
 		i++;
 	}
 	return (i);
 }
 
-char	*ret_str(char *s)
-{
-	char	*str;
-	size_t	len;
-	size_t	i;
-
-	i = 0;
-	len = ft_len_nl(s);
-	str = malloc(sizeof(char) * (len + 2));
-	while (i < len && *(s + i) != '\n' && *(s + i) != '\0')
-	{
-		*(str + i) = *(s + i);
-		i++;
-	}
-	if (*(s + i) == '\n')
-		*(str + i) = '\n';
-//	else if (*(s + i) == '\0')
-//		*(str + i) = '\0';
-	return (str);
-}
-
-int     check_line(char *s)
-{
-	if (!s)
-		return (0);
-        while (*s)
-        {
-                if (*s == '\n' )
-                        return (1);
-                s++;
-        }
-        return (0);
-
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-
-	i = 0;
-	if (dstsize <= 0)
-		return (ft_strlen(src));
-	while (i < dstsize - 1 && src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (ft_strlen(src));
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*str1;
 	size_t	i;
+	size_t	j;
 
-	if (!s1 && !s2)
-		return (NULL);
+	j = ft_strlen(s1);
+	//if (ft_len_nl(s2))
+	//	return (NULL);
 	i = (ft_strlen(s1) + ft_strlen(s2));
 	str1 = (char *)malloc(sizeof(char) * i + 1);
 	if (!str1)
 		return (NULL);
-	while (s1 && *s1)
+	while (s1  && *s1)
+	{
 		*str1++ = *s1++;
-	while (*s2)
+	}
+	while (s2  &&  *s2)
 		*str1++ = *s2++;
 	*str1 = '\0';
 	str1 = str1 - i;
-	return ((char *)str1);
+	if (s1)
+		free(s1 - j);
+//	free(s2);
+	return (str1);
+}
+
+int	ft_chk_stash(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s != NULL && *(s + i))
+	{	
+		if (*(s + i) == '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+char	*ft_strcpy(char *s)
+{
+	char	*ret;
+	int	i;
+
+	i = 0;
+	ret = malloc((sizeof(char) * ft_len_nl(s)) + 1);
+	if (!ret)
+		return (NULL);
+	while (ft_len_nl(s) - i)
+	{
+		*(ret + i) = *(s + i);
+		i++;
+	}
+	ret[i] = '\0';
+	//printf("|strcpy = %s\n|", ret);
+	//printf("ret: %p s: %p\n", ret, s);
+	return (ret);
+}
+
+char	*ft_clear_stash(char *s)
+{
+	char	*ret;
+	int	i;
+
+	i = 0;
+	ret = malloc((sizeof(char) * (ft_strlen(s) - ft_len_nl(s))) + 1);
+	//printf("alloc size: %lu\n", (sizeof(char) * (ft_strlen(s) - ft_len_nl(s)) + 1));
+	if (!ret)
+		return (NULL);
+	//while (s && *(s + i) != '\0')
+	while (s && *(s + i) != '\0' && i < ft_strlen(s) - ft_len_nl(s))
+	{
+		*(ret + i) = *(s + ft_len_nl(s) + i + 1);
+		i++;
+	}
+	//printf("sth: |ret = %p (%s), s = %p (%s)|\n", ret, ret,s,s);
+	if (s)
+		free(s);
+	*(ret + i) = '\0';
+	if (ret[0] == '\0'/* || ret[ft_len_nl(ret)] == '\0'*/)
+	{	
+		free(ret);
+		return (NULL);
+	}
+	return (ret);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*buffer;
-	static char	*str;
 	char	*ret;
+	int	rd;
+	static char	*str[OPEN_MAX];
+	char	*buffer;
 
-	buffer = malloc(BUFFER_SIZE + 1);
-	//buffer = read(fd, buffer, BUFFER_SIZE);
-	//str = "";
-	while (!check_line(str) && read(fd, buffer, BUFFER_SIZE))
+	rd = 1;
+	ret = NULL;
+	if (fd < 0 && BUFFER_SIZE < 0)
+		return (NULL);
+	buffer = malloc((sizeof(char) * BUFFER_SIZE) + 1);
+	while (ft_chk_stash(str[fd]) && rd > 0)
 	{
-		//read(fd, buffer, BUFFER_SIZE);
-		str = ft_strjoin(str, buffer);
-	//	printf("%s", str);
-	//	ret = ret_str(str);	
+		rd = read(fd, buffer, BUFFER_SIZE);
+		buffer[rd] = '\0';
+		if (rd == 0)
+			break;
+		str[fd] = ft_strjoin(str[fd], buffer);
 	}
-	ret = ret_str(str);	
-	//for (size_t i = 0; i < ft_strlen(); i++)
-	//{
-	//	printf("%d %c\n",ret[i], ret[i]);
-	//}
+	free(buffer);
+	if (ft_strlen(str[fd]) > 0)
+		ret = ft_strcpy(str[fd]);
+	else
+	{
+		free(str[fd]);
+		return (NULL);
+		//ret = NULL;
+	}
+	//printf("bef: |ret = %p (%s), s = %p (%s)|\n", ret, ret,str[fd],str[fd]);
+	if (ret)
+		str[fd] = ft_clear_stash(str[fd]);
+	//printf("aft: |ret = %p (%s), s = %p (%s)|\n", ret, ret,str[fd],str[fd]);
 	return (ret);
 }
+/*
+int	main(void)
+{
+	int	fd = 0;
+	char	*str;
+	fd = open("hello2",O_RDONLY);
+	for(int	i = 0; i < 2; i++)
+	{
+		str = get_next_line(fd);
+		printf("%d) result = |%s|", i, str);
+		if(str)
+			free(str);
+	}
+	return (0);
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,33 +6,28 @@
 /*   By: psuanpro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 14:39:39 by psuanpro          #+#    #+#             */
-/*   Updated: 2022/06/08 04:00:17 by psuanpro         ###   ########.fr       */
+/*   Updated: 2022/06/18 01:43:06 by psuanpro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#ifndef BUFFER_SIZE 
-#define BUFFER_SIZE 1
-#endif
-/*
-typedef struct s_line
-{
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <limits.h>
+# include <string.h>
+# ifndef BUFFER_SIZE 
+# define BUFFER_SIZE 5
+# endif
 
-}	t_line;
-*/
-char	*get_next_line(int fd);
-int     check_line(char *s);
-char    *ft_strjoin(char const *s1, char const *s2);
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t  ft_strlen(const char *s);
-char    *ret_str(char *s);
-size_t  ft_len_nl(char *s);
-
+int     ft_len_nl(char *s);
+char    *ft_strjoin(char *s1, char  *s2);
+int     ft_chk_stash(char *s);
+char    *ft_strcpy(char *s);
+char    *ft_clear_stash(char *s);
+char    *get_next_line(int fd);
+int     ft_strlen(char *s);
 #endif
